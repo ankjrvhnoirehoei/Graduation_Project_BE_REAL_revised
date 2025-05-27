cbe72798f512b0797b9d3b932ad15838523b37c2
Nphuc2304 <hnthao2705@gmail.com>
2025-05-27 21:52:57 +0700

    ..

--- src/music/dto/music.dto.ts
+++ src/music/dto/music.dto.ts
@@ -1,9 +1,4 @@
-import { IsMongoId } from 'class-validator';
-
 export class MusicDto {
-  @IsMongoId()
-  readonly postID?: string;
-
   readonly song: string;
   readonly link: string;
   readonly author: string;
