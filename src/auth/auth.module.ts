5fe855cd5d492b80ff78fdaa7f1e62aa6ff0e2ab
Nphuc2304 <hnthao2705@gmail.com>
2025-05-31 03:23:19 +0700

    ..

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,12 +1,15 @@
 import { forwardRef, Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 import { UserModule } from '../user/user.module';
 import { AuthService } from './auth.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from 'src/user/user.schema';
+import { JwtStrategy } from './Middleware/jwt.strategy';
 
 @Module({
   imports: [
+    PassportModule,
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
     forwardRef(() => UserModule),
     JwtModule.register({
@@ -14,7 +17,7 @@ import { User, UserSchema } from 'src/user/user.schema';
       signOptions: { expiresIn: '15m' },
     }),
   ],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
   exports: [AuthService],
 })
 export class AuthModule {}
