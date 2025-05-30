c9c836123198b2bcbcdd445ac0b3fbe7ae1f55e2
Nphuc2304 <hnthao2705@gmail.com>
2025-05-31 01:56:10 +0700

    Update user.module.ts

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from './user.schema';
 import { UserService } from './user.service';
@@ -8,7 +8,7 @@ import { AuthModule } from 'src/auth/auth.module';
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
-    AuthModule,
+    forwardRef(() => AuthModule),
   ],
   providers: [UserService],
   controllers: [UserController],
