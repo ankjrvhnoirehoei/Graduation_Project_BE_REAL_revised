3f3f44a5e1abb28cfc8cf630c81570daf5e28535
Nphuc2304 <hnthao2705@gmail.com>
2025-05-31 01:54:29 +0700

    .

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,11 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { UserModule } from '../user/user.module';
 import { AuthService } from './auth.service';
 
 @Module({
   imports: [
-    UserModule,
+    forwardRef(() => UserModule),
     JwtModule.register({
       secret: 'JWT_SECRET',
       signOptions: { expiresIn: '15m' },
