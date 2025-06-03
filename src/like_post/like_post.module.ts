140799108a74b994503854c527a6f410a6a866e0
ankjrvhnoirehoei <166099409+ankjrvhnoirehoei@users.noreply.github.com>
2025-06-03 12:42:19 +0700

    liked posts

--- src/like_post/like_post.module.ts
+++ src/like_post/like_post.module.ts
@@ -3,11 +3,13 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { PostLike, PostLikeSchema } from './like_post.schema';
 import { PostLikeService } from './like_post.service';
 import { PostLikeController } from './like_post.controller';
+import { Post, PostSchema } from 'src/post/post.schema';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
       { name: PostLike.name, schema: PostLikeSchema },
+      { name: Post.name, schema: PostSchema },
     ]),
   ],
   controllers: [PostLikeController],
