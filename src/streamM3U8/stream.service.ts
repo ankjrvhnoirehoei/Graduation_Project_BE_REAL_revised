865e7c484bc61cb4d88e455ea8d7888c1913cc94
Nphuc2304 <hnthao2705@gmail.com>
2025-06-06 00:16:48 +0700

    cứu

--- src/streamM3U8/stream.service.ts
+++ src/streamM3U8/stream.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import axios from 'axios';
 
 @Injectable()
@@ -7,16 +7,28 @@ export class StreamService {
   private API_TOKEN = process.env.CLOUDFLARE_API_TOKEN;
 
   async createDirectUploadUrl(): Promise<string> {
-    const response = await axios.post(
-      `https://api.cloudflare.com/client/v4/accounts/${this.ACCOUNT_ID}/stream/direct_upload`,
-      {},
-      {
-        headers: {
-          Authorization: `Bearer ${this.API_TOKEN}`,
+    try {
+      const response = await axios.post(
+        `https://api.cloudflare.com/client/v4/accounts/${this.ACCOUNT_ID}/stream/direct_upload`,
+        {},
+        {
+          headers: {
+            Authorization: `Bearer ${this.API_TOKEN}`,
+          },
         },
-      },
-    );
+      );
 
-    return response.data.result.uploadURL;
+      if (response.data.success) {
+        return response.data.result.uploadURL;
+      } else {
+        throw new InternalServerErrorException('Failed to get upload URL');
+      }
+    } catch (error) {
+      console.error(
+        'Error creating direct upload URL:',
+        error.response?.data || error.message,
+      );
+      throw new InternalServerErrorException('Internal server error');
+    }
   }
 }
