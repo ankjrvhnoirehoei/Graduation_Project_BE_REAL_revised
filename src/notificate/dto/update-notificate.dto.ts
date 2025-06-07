6bf1f1242172029628f22ef0e46cc6652625971c
crislk <quybpps40589@gmail.com>
2025-06-07 18:01:53 +0700

    rm NotiResource

--- src/notificate/dto/update-notificate.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-// import { CreateNotificateDto } from './create-notificate.dto';
-
-// export class UpdateNotificateDto extends PartialType(CreateNotificateDto) {}
