c45d501c31e6d7b8b2520d73531d4ffda0f04ca8
Nphuc2304 <hnthao2705@gmail.com>
2025-06-07 16:54:11 +0700

    Update r2.controller.ts

--- src/r2/r2.controller.ts
+++ src/r2/r2.controller.ts
@@ -1,15 +1,22 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { generatePresignedUrl } from './r2.service';
 
 @Controller('r2')
 export class R2UploadController {
-  @Get('presigned-url')
-  async getPresignedUrl(@Query('fileName') fileName: string, @Query('contentType') contentType?: string) {
+  @Post('presigned-url')
+  async getPresignedUrl(
+    @Body() body: { fileName: string; contentType?: string },
+  ) {
+    const { fileName, contentType } = body;
+
     if (!fileName) {
       throw new Error('Missing fileName');
     }
 
-    const url = await generatePresignedUrl(fileName, contentType || 'image/jpeg');
+    const url = await generatePresignedUrl(
+      fileName,
+      contentType || 'image/jpeg',
+    );
     return {
       url,
       fileName,
