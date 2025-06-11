5876c671f95729bb436a567d107a64b394e828f6
Nphuc2304 <hnthao2705@gmail.com>
2025-06-12 00:44:47 +0700

    Update message.module.ts

--- src/message/message.module.ts
+++ src/message/message.module.ts
@@ -1,13 +1,16 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { MessageService } from './message.service';
 import { ChatGateway } from 'src/gateway/chat.gateway';
 import { Message, MessageSchema } from './message.schema';
+import { UserModule } from 'src/user/user.module';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Message.name, schema: MessageSchema }]),
+    forwardRef(() => UserModule),
   ],
   providers: [MessageService, ChatGateway],
+  exports: [MessageService],
 })
 export class MessageModule {}
