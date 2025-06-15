702764a173cbf6a2eac34d17a77d9f3bbc623d5d
Nphuc2304 <hnthao2705@gmail.com>
2025-06-15 14:25:38 +0700

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ import { SwaggerConfig } from '@app/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:3000', 'http://[::1]:4001'],
+    origin: '*',
     credentials: true,
   });
   app.use(cookieParser());
