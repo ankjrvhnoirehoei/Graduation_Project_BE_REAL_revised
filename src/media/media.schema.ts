222dbf4232acbff9816c7aaf4a542a9c191a6af4
Nphuc2304 <hnthao2705@gmail.com>
2025-06-23 00:37:14 +0700

    update post

--- src/media/media.schema.ts
+++ src/media/media.schema.ts
@@ -4,6 +4,23 @@ import { Document, Types } from 'mongoose';
 export type MediaDocument = Media & Document;
 
 @Schema()
+class Tag {
+  @Prop({ type: Types.ObjectId, required: true })
+  userId: Types.ObjectId;
+
+  @Prop({ type: String, required: true })
+  handleName: string;
+
+  @Prop({ type: Number, required: true })
+  positionX: number;
+
+  @Prop({ type: Number, required: true })
+  positionY: number;
+}
+
+const TagSchema = SchemaFactory.createForClass(Tag);
+
+@Schema()
 export class Media {
   @Prop({ type: Types.ObjectId, ref: 'Post', required: true })
   postID: Types.ObjectId;
@@ -13,6 +30,9 @@ export class Media {
 
   @Prop()
   videoUrl?: string;
+
+  @Prop({ type: [TagSchema], default: [] })
+  tags?: Tag[];
 }
 
 export const MediaSchema = SchemaFactory.createForClass(Media);
