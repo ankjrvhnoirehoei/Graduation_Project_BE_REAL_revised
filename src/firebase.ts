6518287af059c3d2f23d593db8a97a177074d821
anhphi1125 <166363173+anhphi1125@users.noreply.github.com>
2025-06-27 14:34:09 +0700

    mở comment

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,8 +1,8 @@
-// import * as admin from 'firebase-admin';
-// const serviceAccount = require('../firebase-service-account.json');
+import * as admin from 'firebase-admin';
+const serviceAccount = require('../firebase-service-account.json');
 
-// admin.initializeApp({
-//   credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
-// });
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
+});
 
-// export { admin };
+export { admin };
